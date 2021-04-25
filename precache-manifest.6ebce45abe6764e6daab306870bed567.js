c4d2e39fd77300e2f73818db1b3f05700733aa5c
Austin Blain <AustinA.Blain@gmail.com>
2021-04-25 14:24:31 -0700

    Updates

---
+++ precache-manifest.6ebce45abe6764e6daab306870bed567.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8799d6f263f7b9218b46",
+    "url": "/wow-classic-survey-august-2019/static/css/main.d970c259.chunk.css"
+  },
+  {
+    "revision": "8799d6f263f7b9218b46",
+    "url": "/wow-classic-survey-august-2019/static/js/main.675b94bb.chunk.js"
+  },
+  {
+    "revision": "3176f3567b747be44b3a",
+    "url": "/wow-classic-survey-august-2019/static/js/runtime~main.7e422c86.js"
+  },
+  {
+    "revision": "a664da6843ae953a0b94",
+    "url": "/wow-classic-survey-august-2019/static/js/2.5a8decf0.chunk.js"
+  },
+  {
+    "revision": "16fd1ac7c81dea24e2e2048317de7213",
+    "url": "/wow-classic-survey-august-2019/index.html"
+  }
+];
